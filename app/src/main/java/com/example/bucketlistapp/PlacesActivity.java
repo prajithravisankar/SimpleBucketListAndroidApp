d34455f5899c4868bebe009702658e2bf85a8093
prajith <prajithravisankar@gmail.com>
2024-08-18 15:54:57 -0700

    2nd commit - initialized string names, description, and images for places to go Activity.

--- app/src/main/java/com/example/bucketlistapp/PlacesActivity.java
+++ app/src/main/java/com/example/bucketlistapp/PlacesActivity.java
@@ -8,12 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class PlacesActivity extends AppCompatActivity {
+
+    ArrayList<BucketList> placesToGo = new ArrayList<>();
+    int[] placesToGoImages = {R.drawable.banff_national_park_canada, R.drawable.disney_world,
+    R.drawable.maldives_ari_atoll_resort, R.drawable.niagra_falls, R.drawable.palouse_washington,
+    R.drawable.santorini_greece, R.drawable.skagit_valley_tulip_field, R.drawable.soneva_jani_maldives,
+    R.drawable.switzerland, R.drawable.vik_beach_iceland, R.drawable.black_sand_beach_iceland};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_places);
+
+
+        setUpPlacesToGoBucketList();
+    }
+
+    private void setUpPlacesToGoBucketList() {
+        String[] names = getResources().getStringArray(R.array.places_names);
+        String[] descriptions = getResources().getStringArray(R.array.places_description);
+
+        for (int i = 0; i < names.length; i++) {
+            placesToGo.add(new BucketList(names[i], descriptions[i], placesToGoImages[i]));
+        }
     }
 }
